e84dd5663d9e9177ac399ec092bdace9008fb4a5
REXXAR-677 <ricardovera71@gmail.com>
2023-09-09 09:57:04 -0500

    fix(ssh): added creds from db-api

---
+++ helpers/utils/request.py
@@ -0,0 +1,25 @@
+import os
+import json
+from dotenv import load_dotenv
+import requests
+from helpers.info.definitions import domain,headers
+
+load_dotenv()
+
+
+def db_request(endpoint: str, data: dict):
+    data["API_KEY"] = os.environ["API_KEY"]
+    payload = json.dumps(data)
+    url = f"{domain}{endpoint}"
+    try:
+        response = requests.post(url, data=payload, headers=headers, verify=False)
+        if response.status_code != requests.codes.ok:
+            return {
+                "error": True,
+                "data": None,
+                "message": f"Request failed with status code: {response.status_code}",
+            }
+        response_json = response.json()
+        return response_json
+    except requests.RequestException as e:
+        return {"error": True, "message": f"An error occurred: {str(e)}", "data": None}
--- helpers/utils/ssh.py
+++ helpers/utils/ssh.py
@@ -1,28 +1,25 @@
-import os
 from time import sleep
 import paramiko
-from dotenv import load_dotenv
-
-from helpers.utils.printer import colorFormatter, log
-
-load_dotenv()
-
+from helpers.utils.printer import log
+from helpers.utils.request import db_request
+from helpers.info.definitions import endpoints
 
 
 def ssh(ip):
     count = 1
-    delay = 1.5
+    delay = 0.85
     conn = paramiko.SSHClient()
     conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     comm = None
     cont = True
+    creds = db_request(endpoints["get_creds"], {})
 
     # Handling multiple SSH sessions
     while cont and count <= 3:
         try:
-            username = os.environ[f"user_{count}"]
-            password = os.environ[f"password_{count}"]
-            port = os.environ["port"]
+            username = creds["data"][count]["user_name"]
+            password = creds["data"][count]["password"]
+            port = 22
             conn.connect(ip, port, username, password)
             comm = conn.invoke_shell()
             cont = False
@@ -31,7 +28,7 @@ def ssh(ip):
             count += 1
             continue
         break
-    
+
     def enter():
         comm.send(" \n")
         comm.send(" \n")
@@ -42,14 +39,14 @@ def ssh(ip):
         sleep(delay)
         enter()
 
-    def quit():
+    def quit_ssh():
         conn.close()
 
-    if ip == "181.232.180.5" or ip == "181.232.180.6" or ip == "181.232.180.7":
+    if ip in ["181.232.180.5", "181.232.180.6", "181.232.180.7"]:
         command("enable")
         command("config")
         command("scroll 512")
     else:
         command("sys")
 
-    return (comm, command, quit)
+    return (comm, command, quit_ssh)
